1b93e0542670d557824e6843763aaa3e797bed5f
wangfei-824 <651277897@qq.com>
2019-07-21 09:17:38 +0800

    Add project 05-USART_printf. Use uart1 and uart2 to communicate with PC.

---
+++ 05-USART_printf/MDK-ARM/RTE/_05-USART_printf/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '05-USART_printf' 
+ * Target:  '05-USART_printf' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
